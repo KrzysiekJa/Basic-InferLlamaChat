06d560bd623c07b7de8e3158cb0668b6c10d4f96
KrzysiekJa <kjmj77@gmail.com>
2025-09-11 00:21:26 +0200

    Added rate limiter for endpoints (`slowapi`). Defined in separate py file. Added `request` parameters to endpoints' functions for limiter usage.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ from fastapi import FastAPI, status
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 
+from app.rate_limiting import limiter
 from app.config import settings
 from app.api import register_routes
 
@@ -13,22 +16,28 @@ TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
 
 app = FastAPI(title="Llama4Infer ChatApp")
-
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 register_routes(app)
 
 
 @app.get("/", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
-async def root():
+@limiter.limit("30/minute")
+async def root(request: Request):
     return RedirectResponse(url="/ui")
 
 
 @app.get("/ui", status_code=status.HTTP_200_OK)
+@limiter.limit("30/minute")
 async def ui(request: Request):
-    return TEMPLATES.TemplateResponse("index.html", {
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {
             "request": request,
             "minOutTokens": settings.chat.OUTPUT_MIN_TOKENS,
             "maxOutTokens": settings.chat.OUTPUT_MAX_TOKENS,
-        },)
+        },
+    )
 
 
 if "__main__" == __name__:
