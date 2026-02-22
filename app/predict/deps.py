8e217b09b4241297a0bbe9e54e6d629ddf5ce0f5
KrzysiekJa <kjmj77@gmail.com>
2026-02-22 21:30:05 +0100

    Added safe async generator cleanup in .

--- app/predict/deps.py
+++ app/predict/deps.py
@@ -1,5 +1,6 @@
 from typing import AsyncGenerator
 from openai import AsyncOpenAI
+import contextlib
 
 from app.config import settings
 
@@ -16,14 +17,17 @@ async def stream_generator(
 ) -> AsyncGenerator[str, None]:
     tokens_count = 0
 
-    async for chunk in response:
-        if not chunk.choices:
-            break
+    async with contextlib.aclosing(response) as resp:
+        # Added extra `with` block for safety reasons - for generator cleanup.
+        # It awaits generator's `aclose` method on the way out.
+        async for chunk in resp:
+            if not chunk.choices:
+                break
 
-        content = chunk.choices[0].delta.content
-        tokens_count += len(content.split())
+            content = chunk.choices[0].delta.content
+            tokens_count += len(content.split())
 
-        if settings.chat.OUTPUT_MAX_TOKENS <= tokens_count:
-            break
+            if settings.chat.OUTPUT_MAX_TOKENS <= tokens_count:
+                break
 
-        yield content
+                yield content
